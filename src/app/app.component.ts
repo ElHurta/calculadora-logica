82256b602865b21f7e23c86b950b234635b49d1a
Hurta08 <juan.david.hurtado08@gmail.com>
2022-05-10 11:43:54 -0500

    feature: displaying used symbols

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TokenServiceService } from './services/token-service.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  actualInput: string = '';
+  foundSimbols: any = [];
+  currentSimbols: any;
 
   constructor( private jsonService: TokenServiceService ) {}
 
-  actualInput: string = '';
-  currentSimbols: string[] = [];
+  ngOnInit() {
+    this.jsonService.getTokens().subscribe(data => {
+      this.currentSimbols = data;
+    }
+    );
+  }
 
   typeInput(event: any) {
-    console.log(event.target.value);
     this.actualInput += event.target.value;
   }
 
   showSimbols(){
-    this.jsonService.getTokens().subscribe(
-      (data: any) => {
-        this.currentSimbols = data;
+    this.foundSimbols = [];
+    for(const key in this.currentSimbols) {
+      if(this.currentSimbols.hasOwnProperty(key)) {
+          var value = this.currentSimbols[key];
+          if(this.actualInput.includes(value)) {
+            this.foundSimbols.push({
+              key: key,
+              value: value
+            });
+          }
       }
-    );
-    
-    console.log(this.currentSimbols);
+    }
+    console.log(this.foundSimbols);
   }
 }
